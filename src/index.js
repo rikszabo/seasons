fde98e4817b8fad6b35a00f8321160b6a3dca851
Patrik <szabo.patrik@e-vista.hu>
2019-09-12 12:32:02 +0200

    add style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  //constructor(props) {
+  //super(props);
+  //This is the only time we do direct assignment to this.state
+  //this.state = { lat: null, errorMessage: '' };
+  //}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount() {
+    console.log('My component was rendered to the screen');
+    //Geolocation API
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        //we called set state
+        this.setState({ lat: position.coords.latitude });
+
+        //we did not
+        //this.state.lat = position.coords.latitude;
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered');
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    console.log('render run');
+    return <div>Loading!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
